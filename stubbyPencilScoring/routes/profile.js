e8f635a077b1926c349234824bede9f119f25cf8
Kristin Sztengel <kcsztengel@gmail.com>
2016-10-07 15:51:44 -0600

    profile.js renders on profile.hbs

--- stubbyPencilScoring/routes/profile.js
+++ stubbyPencilScoring/routes/profile.js
@@ -4,18 +4,18 @@ var router = express.Router();
 var knex = require('../db/knex')
 
 router.get('/', function(req, res, next) {
-  knex('games')
-    .then(games => {
-      res.render('profile',{games})
-      })
+  // knex('games')
+  //   .then(games => {
+  //     res.render('profile',{games})
+  //     })
   knex('users')
     .then(users => {
     res.render('profile',{users})
-  knex('rounds')
-  .then(rounds =>{
-    res.render('profile',{rounds})
-
-    })
+  // knex('rounds')
+  // .then(rounds =>{
+  //   res.render('profile',{rounds})
+  //
+  //   })
 
   })
 })
--- stubbyPencilScoring/routes/scorecard.js
+++ stubbyPencilScoring/routes/scorecard.js
@@ -4,21 +4,20 @@ var router = express.Router();
 var knex = require('../db/knex')
 
 router.get('/', function(req, res, next) {
-  knex('games')
-    .then(games => {
-      res.render('scorecard',{games})
-      })
+  // knex('games')
+  //   .then(games => {
+  //     res.render('scorecard',{games})
+  //     })
   knex('users')
     .then(users => {
     res.render('scorecard',{users})
-  knex('rounds')
-  .then(rounds =>{
-    res.render('scorecard',{rounds})
+  // knex('rounds')
+  // .then(rounds =>{
+  //   res.render('scorecard',{rounds})
 
-    })
+    // })
 
   })
 })
 
 module.exports = router;
- 
