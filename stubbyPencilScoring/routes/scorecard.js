e21d3a18c1189546f9b88194dc65516e2f7ae6d6
Kristin Sztengel <kcsztengel@gmail.com>
2016-10-07 15:45:33 -0600

    scorecard.js renders on scorecard.hbs

--- stubbyPencilScoring/routes/scorecard.js
+++ stubbyPencilScoring/routes/scorecard.js
@@ -1,8 +1,24 @@
+'use strict'
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex')
 
 router.get('/', function(req, res, next) {
-    res.render('scorecard')
+  knex('games')
+    .then(games => {
+      res.render('scorecard',{games})
+      })
+  knex('users')
+    .then(users => {
+    res.render('scorecard',{users})
+  knex('rounds')
+  .then(rounds =>{
+    res.render('scorecard',{rounds})
+
+    })
+
+  })
 })
 
 module.exports = router;
+ 
