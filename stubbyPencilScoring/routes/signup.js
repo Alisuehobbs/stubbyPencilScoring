d19bcaaba3d9ba6d59812352d9f4da639aa84706
Ali Hobbs <alisuehobbs@gmail.com>
2016-10-07 16:58:46 -0600

    routes work on signup

--- stubbyPencilScoring/routes/signup.js
+++ stubbyPencilScoring/routes/signup.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('signup');
+router.get('/', (req, res, next) => {
+    res.render('signup');
 });
+
+router.post('/', (req, res, next) => {
+
+  const newUserObj = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    user_name: req.body.user_name,
+    image: req.body.image,
+    email: req.body.email,
+    hashed_password: req.body.password
+  }
+
+    knex('users')
+      .insert(newUserObj)
+      .then(() => {
+        res.redirect('profile')
+      })
+})
 
 module.exports = router;
