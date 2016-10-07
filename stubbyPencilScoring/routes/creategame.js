df763240d450300acbb475f3196f81b2e90e88d0
Matt Gardner <matthewlgardner@gmail.com>
2016-10-07 17:04:00 -0600

    added form to creatgame.js

--- stubbyPencilScoring/routes/creategame.js
+++ stubbyPencilScoring/routes/creategame.js
@@ -1,15 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const knex = require('../db/knex')
 
 
-router.get('/', function(req, res, next) {
+router.get('/', ((req, res, next) => {
     res.render('createGame')
-})
+}))
 
+// insert game criteria into database
+router.post('/', (req, res) => {
+    console.log('you hit the post route');
+    console.log('req.body', req.body);
+    // knex('games')
+    //     .insert({
+    //         game_name: req.body.game_name,
+    //         status_id: 1
+    //         user_id: //hard code for now(req.session.id) or localStorage.getItem('id')
+    //     })
+    //     .returning('*')
+    //     .then(gameInsertion => {
+    //         var gameThign = gameInsertion
+    //         console.log('id', gameInsertion);
+    //         // knex('rounds')
+    //         .insert({
+    //             games_id: gameInsertion.id,
+    //
+    //         })
+    //
+    //     })
+})
 
 /* GET scorecard page. */
 // router.post('/', function(req, res, next) {
 //   res.render('scorecard', { title: 'this is the scorecard page' });
 // });
 
-module.exports = router;;
+module.exports = router;
