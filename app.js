79b5facfb10249a68f4acaff081b1beae111e5eb
Kristin Sztengel <kcsztengel@gmail.com>
2016-10-09 17:52:03 -0600

    deploying AGAIN

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+'use strict'
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config();
+// }
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
-const logger = require('morgan');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session')
-const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+switch (app.get('env')) {
+    case 'development':
+        app.use(morgan('dev'));
+        break;
+
+    case 'production':
+        app.use(morgan('short'));
+        break;
+
+    default:
+}
 
 const index = require('./routes/index')
 const signup = require('./routes/signup')
@@ -13,7 +30,6 @@ const profile = require('./routes/profile')
 const creategame = require('./routes/creategame')
 const scorecard = require('./routes/scorecard')
 
-const app = express();
 
 
 // view engine setup
@@ -22,14 +38,15 @@ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(cookieSession({
     name: 'stubbypencilscoring',
-    secret: 'secretsecretivegotasecret'
+    secret: 'secretsecretivegotasecret',
+    secureProxy: app.get('env') === 'production'
 }))
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
