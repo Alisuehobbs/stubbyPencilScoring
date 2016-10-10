85f3218c4ad9a92c7b52a24b8fc2812b94e739f1
Kristin Sztengel <kcsztengel@gmail.com>
2016-10-09 18:01:51 -0600

    ah, spelled process.env wrong

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieSession({
     name: 'stubbypencilscoring',
-    secret: processs.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET,
     secureProxy: app.get('env') === 'production'
 }))
 app.use(cookieParser());
