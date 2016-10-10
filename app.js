a24a715d154bad3c77d365bc19e686394c94bd94
Kristin Sztengel <kcsztengel@gmail.com>
2016-10-09 18:13:51 -0600

    trying to lose the favicon

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // }
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session')
