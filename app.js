32890ce4b7d674599eb2cfb9da7939ec28b7618a
Kristin Sztengel <kcsztengel@gmail.com>
2016-10-10 10:27:42 -0600

    required dotenv

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config();
-// }
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const path = require('path');
 // const favicon = require('serve-favicon');
