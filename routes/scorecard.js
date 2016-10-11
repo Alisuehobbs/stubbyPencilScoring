f5b6aa5826692b5aa5bb6c954f0c23da5392cb08
Kristin Sztengel <kcsztengel@gmail.com>
2016-10-11 09:18:00 -0600

    user_rounds

--- routes/scorecard.js
+++ routes/scorecard.js
@@ -7,17 +7,18 @@ router.get('/', function(req, res, next) {
 
   knex('users')
   .join('games', 'users_id', 'games.users_id')
-  // .join('user_rounds','games.id', 'user_rounds.games_id')
-  .where('games.users_id', 1)
+  .join('rounds', 'games.users_id', 'rounds.users_id')
+  .join('user_rounds','games.users_id', 'user_rounds.users_id')
+  .where('games.users_id', 4)
   // .where('user_id', req.session.user_id)
 
   .then(scorecard => {
       res.render('scorecard',{
         scorecard: scorecard,
         game_name: scorecard[0].game_name,
-        image: scorecard.image,
-        number_of_rounds: scorecard[0].number_of_rounds,
-        label: scorecard[0].label
+        image: scorecard[0].image,
+        label: scorecard[0].label,
+        rounds_id: scorecard[0].rounds_id
         })
       console.log("scorecard", scorecard);
       })
