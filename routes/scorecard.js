1a086734bd03911d8705b2fa25152d4db6ba7663
Matt Gardner <matthewlgardner@gmail.com>
2016-10-11 13:58:57 -0600

    changed redirect in creategame.js to '/scorecard'

--- routes/scorecard.js
+++ routes/scorecard.js
@@ -16,38 +16,38 @@ var knex = require('../db/knex')
 //
 
 router.get('/', function(req, res, next) {
-  // let numRounds = Number.parseInt (req.body.number_of_rounds)
-  // console.log(numRounds);
-  // function insertRows(numRounds) {
-  //     for (var i = 0; i <= numRounds; i++) {
-  //         knex('user_rounds')
-  //             .where('games.users_id', 1)
-  //             .catch((err) => {
-  //                 return err
-  //             })
-  //     }
-  // }
+    // let numRounds = Number.parseInt (req.body.number_of_rounds)
+    // console.log(numRounds);
+    // function insertRows(numRounds) {
+    //     for (var i = 0; i <= numRounds; i++) {
+    //         knex('user_rounds')
+    //             .where('games.users_id', 1)
+    //             .catch((err) => {
+    //                 return err
+    //             })
+    //     }
+    // }
 
-  knex('users')
-  .join('games', 'users_id', 'games.users_id')
-  .join('rounds', 'games.users_id', 'rounds.users_id')
-  // .join('user_rounds','games.users_id', 'user_rounds.users_id')
-  .select('user_id as userId')
-  .where('games.users_id', 1)
-  // .where('user_id', req.session.user_id)
+    knex('users')
+        .join('games', 'users_id', 'games.users_id')
+        .join('rounds', 'games.users_id', 'rounds.users_id')
+        // .join('user_rounds','games.users_id', 'user_rounds.users_id')
+        // .select('user_id as userId')
+        .where('games.users_id', 1)
+        // .where('user_id', req.session.user_id)
 
-  .then(scorecard => {
+    .then(scorecard => {
 
-      res.render('scorecard',{
-        scorecard: scorecard,
-        // game_name: scorecard[0].game_name,
-        // image: scorecard[0].image,
-        // label: scorecard[0].label,
-        // rounds_id: scorecard[0].rounds_id
+        res.render('scorecard', {
+            scorecard: scorecard,
+            // game_name: scorecard[0].game_name,
+            // image: scorecard[0].image,
+            // label: scorecard[0].label,
+            // rounds_id: scorecard[0].rounds_id
 
         })
-      console.log('scorecard', scorecard);
-      })
+        console.log('scorecard', scorecard);
     })
+})
 
 module.exports = router;
