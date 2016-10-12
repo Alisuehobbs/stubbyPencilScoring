a59af58341c47f7f06762f240811f340c53eca43
Ali Hobbs <alisuehobbs@gmail.com>
2016-10-12 12:57:18 -0600

    started working on login

--- routes/profile.js
+++ routes/profile.js
@@ -15,18 +15,19 @@ const authorize = (req, res, next) => {
 }
 
 router.get('/', authorize, (req, res, next) => {
-    knex('users')
-        .join('games', 'users.id', 'games.users_id')
-        // .join('rounds','games_id', 'rounds.games_id')
-        .where('users.id', 1)
-        .then(profile => {
-            res.render('profile', {
-                profile: profile,
-                game_name: profile[0].game_name,
-                image: profile[0].image
-            })
-            console.log('profile', profile);
-        })
+  res.send('You made it to the profile page')
+    // knex('users')
+    //     .join('games', 'users.id', 'games.users_id')
+    //     // .join('rounds','games_id', 'rounds.games_id')
+    //     .where('users.id', 1)
+    //     .then(profile => {
+    //         res.render('profile', {
+    //             profile: profile,
+    //             game_name: profile[0].game_name,
+    //             image: profile[0].image
+    //         })
+    //         console.log('profile', profile);
+    //     })
 })
 
 
